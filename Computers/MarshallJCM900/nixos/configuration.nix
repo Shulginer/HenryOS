bd742e3ba19bd69a6d3aa494245bc5b8349ab58a
Henry Crusher <henry.crusher.oshulginer@gmail.com>
2026-08-12 18:50:00 -0500

    chore(configuration.nix): Remove unnecessary comment - Remove unnecessary comment originally in example configuration.nix

--- Computers/MarshallJCM900/nixos/configuration.nix
+++ Computers/MarshallJCM900/nixos/configuration.nix
@@ -161,7 +161,7 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-    vis # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vis     
     wget
     curl
     git
